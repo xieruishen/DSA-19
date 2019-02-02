12123e2b4f1497d114f7409813eee752df009fff
xieruishen <xierui.shen@students.olin.edu>
2019-02-02 17:26:53 -0500

    finish my linked list

--- datastructures/day02/src/your_code/MyLinkedList.java
+++ datastructures/day02/src/your_code/MyLinkedList.java
@@ -25,7 +25,9 @@ public class MyLinkedList {
     }
 
     public MyLinkedList() {
-        // TODO
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
     }
 
     public int size() {
@@ -45,30 +47,103 @@ public class MyLinkedList {
     }
 
     public void addLast(Chicken c) {
-        // TODO
+        if (size != 0){
+            Node n1 = new Node(c,tail,null);
+            tail.next = n1;
+            tail = n1;
+        }
+        else {
+            Node n1 = new Node(c);
+            tail = n1;
+            head = n1;
+        }
+        size ++;
     }
 
     public void addFirst(Chicken c) {
-        // TODO
+        if (size != 0){
+            Node n1 = new Node(c, null, head);
+            head.prev = n1;
+            head = n1;
+        }
+        else{
+            Node n1 = new Node(c);
+            head = n1;
+            tail = n1;
+        }
+        size ++;
     }
 
     public Chicken get(int index) {
-        // TODO
-        return null;
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException();
+        }
+        Node node = head;
+        for (int i=0;i<index; i++){
+            node = node.next;
+        }
+        return node.val;
     }
 
     public Chicken remove(int index) {
-        // TODO
-        return null;
+        Chicken bad;
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException();
+        }
+        else if (index == 0){
+            bad = removeFirst();
+        }
+        else if (index == size - 1){
+            bad = removeLast();
+        }
+        else{
+            Node escapes = head;
+            for (int i=0;i<index; i++){
+                escapes = escapes.next;
+                escapes.prev.next = escapes.next;
+                escapes.next.prev = escapes.prev;
+            }
+            size --;
+            bad = escapes.val;
+        }
+        return bad;
     }
 
     public Chicken removeFirst() {
-        // TODO
-        return null;
+        Node escapes;
+        if (size == 0){
+            return null;
+        }
+        else if(size == 1){
+            escapes = head;
+            head = null;
+            tail = null;
+        }
+        else{
+            escapes = head;
+            head = escapes.next;
+            head.prev = null;
+        }
+        size --;
+        return escapes.val;
     }
 
     public Chicken removeLast() {
-        // TODO
-        return null;
+        Node escapes;
+        if (size == 0){
+            return null;
+        }
+        else if(size == 1){
+            escapes = tail;
+            head = null;
+            tail = null;
+        }
+        else{
+            escapes = tail;
+            tail = escapes.prev;
+            tail.next = null;
+        }
+        size --;
+        return escapes.val;
     }
 }
